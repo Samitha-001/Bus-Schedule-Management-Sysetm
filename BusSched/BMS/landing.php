0ce68fad71ad9bf007b9fcda96488ebdba810572
Samitha Rathnayake <106796575+Samitha-001@users.noreply.github.com>
2022-12-04 10:22:23 +0530

    landing page has been styled

---
+++ BusSched/BMS/landing.php
@@ -0,0 +1,19 @@
+<?php
+    require 'includes/header.php';
+?>
+<style>
+    <?php 
+        include("css/landing.css"); 
+    ?>
+</style>
+
+<div class="find_bus">
+    <h1>Find A Bus</h1>
+    <label for="from">From</label>
+    <input type="text" name="" id="from" placeholder="Choose city">
+    <label for="to">To</label>
+    <input type="text" name="" id="to" placeholder="Choose city">
+    <input type="button" value="Find">
+</div>
+</body>
+</html>
