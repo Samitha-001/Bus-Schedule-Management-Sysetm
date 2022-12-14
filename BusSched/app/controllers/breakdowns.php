1578cb316218f5e815c00a3816f682853cd8b957
Maleesha Madhuhansani <maleemapalagama@gmail.com>
2022-12-14 10:58:00 +0530

    Conductor signup and login

---
+++ BusSched/app/controllers/breakdowns.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class Breakdowns {
+
+    use Controller;
+
+    public function index() {
+        $breakdown = new Breakdown();
+        $breakdowns = $breakdown->getBreakdowns();
+
+        $data = [];
+        if($_SERVER['REQUEST_METHOD'] == "POST") {
+            $_POST['bus_no'] = strtoupper($_POST['breakdown_no']);
+            if($breakdown->validate($_POST)) {
+                $breakdown->insert($_POST);
+
+                redirect('breakdowns');
+            }
+
+            $data['errors'] = $breakdown->errors;
+        }
+
+		$this->view('breakdown', ['breakdowns' => $breakdowns]);
+    }
+}
