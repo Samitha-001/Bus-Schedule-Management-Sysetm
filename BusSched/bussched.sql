fc8c0d28eb6a7ee09283b33dcbea4ab76bb88996
Venudi <63283896+Venudi@users.noreply.github.com>
2023-03-11 13:30:04 +0530

    modified bus table

--- BusSched/bussched.sql
+++ BusSched/bussched.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2023 at 08:32 AM
+-- Generation Time: Mar 11, 2023 at 08:58 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.4.19
 
