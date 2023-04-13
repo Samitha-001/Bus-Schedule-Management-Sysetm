de0e1c51a10c1e12927a927685d1aac08a34dfa0
Venudi <63283896+Venudi@users.noreply.github.com>
2023-04-13 22:10:52 +0530

    Passenger info edit

--- BusSched/public/assets/js/passengerprofile.js
+++ BusSched/public/assets/js/passengerprofile.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let savebtn = document.getElementById("save-passenger-info");
     // event listener to save button
     savebtn.addEventListener("click", function (e) {
-        // e.preventDefault();
+        e.preventDefault();
         saveRow(e);
     });
 
@@ -119,111 +119,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
         }
     }
-
-    // let editbtn = document.getElementById("edit-passenger-info");
-
-    // editbtn.addEventListener("click", function (e) { 
-    //     editRow(e);
-    // });
-    
-    // // save passenger info edit
-    // let savebtn = document.getElementById("save-passenger-info");
-    // // event listener to save button
-    // savebtn.addEventListener("click", function (e) {
-    //     saveRow(e);
-    // });
-
-    // // cancel passenger info edit
-    // let cancelbtn = document.getElementById("cancel-passenger-info");
-    
-    // // add event listener to cancel button
-    // cancelbtn.addEventListener("click", function (e) {
-    //     cancelEdit(e);
-    // });
-
-    // // function to autofill and display editing row
-    // function editRow(e) {
-    //     let ticketdiv = e.target.parentElement.parentElement;
-    //     let userdetails = ticketdiv.querySelectorAll("p");
-    //     let username = userdetails[0].getAttribute("data-username");
-    //     console.log("username: "+username);
-    //     let name = userdetails[0].textContent.trim();
-    //     let phone = userdetails[1].textContent.trim();
-    //     let address = userdetails[2].textContent.trim();
-    //     let dob = userdetails[3].textContent.trim();
-
-    //     // get the form inside ticketdiv
-    //     let infoform = ticketdiv.querySelector("form");
-
-    //     // get element by class
-    //     let profileinfo = ticketdiv.querySelector("div.info-grid");
-
-    //     // inputs
-    //     let inputs = infoform.querySelectorAll("input");
-
-    //     inputs[0].value = name;
-    //     inputs[1].value = phone;
-    //     inputs[2].value = address;
-    //     inputs[3].value = dob;
-    //     inputs[4].value = username;
-
-    //     profileinfo.style.display = "none";
-    //     infoform.style.display = "grid";
-    // }
-
-    // // save edit
-    // function cancelEdit(e) {
-    //     let infoform = e.target.parentElement.parentElement;
-    //     infoform.style.display = "none";
-
-    //     let ticketdiv = e.target.parentElement.parentElement.parentElement;
-    //     let profileinfo = ticketdiv.querySelector(".info-grid");
-    //     profileinfo.style.display = "grid";
-    // }
-
-    // // save edited info
-    // function saveRow(e) {
-    //     let ticketdiv = e.target.parentElement.parentElement.parentElement;
-    //     let prevValues = ticketdiv.querySelectorAll("p");
-    //     let data = {}
-    //     let inputs = ticketdiv.querySelectorAll("input");
-
-    //     data["username"] = inputs[4].value;
-    //     console.log("username: "+data["username"]);
-    //     // check values of inputs against previous values
-    //     for (let i = 0; i < inputs.length-1; i++) {
-    //         if (inputs[i].value !== prevValues[i].textContent.trim()) {
-    //             data[inputs[i].name] = inputs[i].value;
-    //             console.log("data: "+data[inputs[i].name]);
-    //         }
-    //     }
-
-    //     // check if data is not empty
-    //     if (Object.keys(data).length > 1) {
-    //         console.log("Data below")
-    //         console.log(data)
-    //         // send data to server
-
-    //         fetch(`${ROOT}/passengerprofile/api_edit`, {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json;charset=utf-8",
-    //             },
-    //             body: JSON.stringify(data),
-    //         })
-    //         .then((res) => res.json())
-    //         .catch((error) => console.log(error))
-    //         .then((data) => {
-    //             console.log(data);
-    //         });
-        
-    //     }
-
-    //     let infoform = ticketdiv.querySelector("form");
-    //     infoform.style.display = "none";
-    //     let profileinfo = ticketdiv.querySelector(".info-grid");
-    //     profileinfo.style.display = "grid";
-    // }
 
     // GIFT POINTS
     let giftPointsDiv = document.getElementById("gift-points-div");
