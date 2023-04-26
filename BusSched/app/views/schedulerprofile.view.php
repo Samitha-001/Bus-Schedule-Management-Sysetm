a285f697d6ff22b1813b28b1463f0fe712195921
Samitha <rathnayakesamitha1317@gmail.com>
2023-04-26 17:51:17 +0530

    scheduler profile edit options

--- BusSched/app/views/schedulerprofile.view.php
+++ BusSched/app/views/schedulerprofile.view.php
@@ -1,3 +1,19 @@
+<!-- <?php
+
+$username = $_SESSION['USER']->username;
+if (isset($_SESSION['USER'])) {
+    if ($_SESSION['USER']->role == 'driver') {
+        redirect('drivers');
+    } else if ($_SESSION['USER']->role == 'conductor') {
+        redirect('conductors');
+    } else if ($_SESSION['USER']->role == 'owner') {
+        redirect('owners');
+    } else if ($_SESSION['USER']->role == 'admin') {
+        redirect('admins');
+    }
+}
+?> -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +24,9 @@
 
     <link href="<?= ROOT ?>/assets/css/style2.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/schedsidebar.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/schedulerprofile.css">
     <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
+    <script src="<?= ROOT ?>/assets/js/schedulerprofile.js"></script>
     <title>Profile</title>
 </head>
 
@@ -16,64 +34,60 @@
     <?php 
         include "components/navbar_new.php";
         include "components/schedulersidebar.php";
+
+        $scheduler = $data[0];
+        $username = $scheduler->username;
+    
     ?>
 
     
 
         <div class="row">
         
-        <div class="card-container-sched" id="info-card-sched">
-            <ul>
-                <p style="font-size: 32px;">Personal Info</p>
-                <table class="styled-table">
-                    <tr>
-                        <th>Username: </th>
-                        <td>
-                            <?= $_SESSION['USER']->username ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Name: </th>
-                        <td>
-                            Scheduler
-                        </td>
-                    <tr>
-                        <th>Email: </th>
-                        <td>
-                            <?= $_SESSION['USER']->email ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Phone: </th>
-                        <td>
-                        <div style="position: relative;">
-  <input type="text" id="number" name="phone" value="Enter the number" style="padding-right: 30px;">
-  <img src="<?= ROOT ?>/assets/images/icons/Edit.png" style="position: absolute; right: 5px; top: 50%; transform: translateY(-50%); width: 20px">
-</div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Address: </th>
-                        <td>
-                        <div style="position: relative;">
-  <input type="text" id="addr" name="addr" value="Enter the address" style="padding-right: 30px;">
-  <img src="<?= ROOT ?>/assets/images/icons/Edit.png" style="position: absolute; right: 5px; top: 50%; transform: translateY(-50%); width: 20px">
-</div>
-
-                        </td>
-                    </tr>
-                </table>
-            </ul>
-        </div>
-        <div class="profile-picture-container">
-        <div class="profile-picture">
-            
-            <img src="profile_picture.jpg" alt="Profile Picture">
-            <div class="overlay">
-                <span class="plus-icon">+</span>
+        <div class="nav-cards">
+        <div class="passenger-profile-card">
+            <div class="info-grid" style="padding-left:5px;">
+                <h1>Username:</h1>
+                <p data-username="<?= $username ?>">
+                    <?= $scheduler->username ?>
+                </p>
+                <h1>Name:</h1>
+                <p>
+                    <?= $scheduler->name ?>
+                </p>
+                
+                <h1>Phone:</h1>
+                <p>
+                    <?= $scheduler->phone ?>
+                </p>
+                <h1>Address:</h1>
+                <p>
+                    <?= $scheduler->address ?>
+                </p>
+                
+                <button href=# id='edit-passenger-info'>Edit</button>
             </div>
+
+            <!-- edit form for passenger info -->
+            <form style="padding-left:5px;" class='edit-info-form info-grid'>
+                <h1>Username:</h1>
+                <input type="text" name="username" id="username" value="<?= $scheduler->username ?>">
+                <h1>Name:</h1>
+                <input type="text" name="name" id="name" value="<?= $scheduler->name ?>">
+                <h1>Phone:</h1>
+                <input type="text" name="phone" id="phone" value="<?= $scheduler->phone ?>">
+                <h1>Address:</h1>
+                <input type="text" name="address" id="address" value="<?= $scheduler->address ?>">
+                
+                
+                <!-- TODO -->
+                <div class="info-grid-start-2">
+                    <button id='save-passenger-info'>Save</button>
+                    <button id='cancel-passenger-info'>Cancel</button>
+                </div>
+            </form>
+
         </div>
-    `   </div>
         </div>
 
 </body>
---
+++ BusSched/public/assets/js/schedulerprofile.js
@@ -0,0 +1,116 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let editbtn = document.getElementById("edit-passenger-info");
+  
+    editbtn.addEventListener("click", function (e) {
+      editRow(e);
+    });
+  
+    // save passenger info edit
+    let savebtn = document.getElementById("save-passenger-info");
+    // event listener to save button
+    savebtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      saveRow(e);
+    });
+  
+    // cancel passenger info edit
+    let cancelbtn = document.getElementById("cancel-passenger-info");
+  
+    // add event listener to cancel button
+    cancelbtn.addEventListener("click", function (e) {
+      cancelEdit(e);
+    });
+  
+    // function to autofill and display editing row
+    function editRow(e) {
+      let ticketdiv = e.target.parentElement.parentElement;
+      let userdetails = ticketdiv.querySelectorAll("p");
+      let name = userdetails[0].textContent.trim();
+      let phone = userdetails[1].textContent.trim();
+      let address = userdetails[2].textContent.trim();
+      let dob = userdetails[3].textContent.trim();
+  
+      // get the form inside ticketdiv
+      let infoform = ticketdiv.querySelector("form");
+  
+      // get element by class
+      let profileinfo = ticketdiv.querySelector("div.info-grid");
+  
+      // inputs
+      let inputs = infoform.querySelectorAll("input");
+  
+      inputs[0].value = name;
+      inputs[1].value = phone;
+      inputs[2].value = address;
+      inputs[3].value = dob;
+  
+      profileinfo.style.display = "none";
+      infoform.style.display = "grid";
+    }
+  
+    // cancel edit
+    function cancelEdit(e) {
+      let infoform = e.target.parentElement.parentElement;
+      infoform.style.display = "none";
+  
+      let ticketdiv = e.target.parentElement.parentElement.parentElement;
+      let profileinfo = ticketdiv.querySelector(".info-grid");
+      profileinfo.style.display = "grid";
+    }
+  
+    // save edited info
+    function saveRow(e) {
+      // get div
+      let ticketdiv = e.target.parentElement.parentElement.parentElement;
+      let prevValues = ticketdiv.querySelectorAll("p");
+      let id = document.querySelector("#username").innerHTML;
+      // Hi passenger1!
+      //get username from above
+      id = id.substring(3, id.length - 1);
+      let data = { id: id };
+      let inputs = ticketdiv.querySelectorAll("input");
+  
+      // check values of inputs against previous values
+      for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value !== prevValues[i].textContent.trim()) {
+          data[inputs[i].name] = inputs[i].value;
+        }
+      }
+  
+      // check if data is not empty
+      if (Object.keys(data).length !== 0) {
+        // send data to server
+        let url = `${ROOT}/passengerprofile/api_edit`;
+        let options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        };
+        fetch(url, options)
+          .then((response) => response.json())
+          .then((data) => {
+            // console.log(data);
+            for (let i = 0; i < inputs.length; i++) {
+              prevValues[i].textContent = inputs[i].value;
+            }
+            // hide form and show info
+            let infoform = ticketdiv.querySelector("form");
+            infoform.style.display = "none";
+            let profileinfo = ticketdiv.querySelector(".info-grid");
+            profileinfo.style.display = "grid";
+            // if (data.success) {
+            //     // this doesn't get called
+            //     // update the values
+            // } else {
+            //     // alert(data.message);
+            // }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }  
+  });
+  
