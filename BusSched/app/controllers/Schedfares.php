458d6ae4ae2d91b75ab64870e2d64a98d63efc41
Samitha <rathnayakesamitha1317@gmail.com>
2023-05-08 17:30:30 +0530

    Sheduler fares

--- BusSched/app/controllers/Schedfares.php
+++ BusSched/app/controllers/Schedfares.php
@@ -7,38 +7,43 @@ class Schedfares
     use Controller;
 
     public function index()
-    {
-        $schedfare = new Fareinstance();
-        $schedfares = $schedfare->getFareInstances();
-        $data = [];
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            
+    {   
 
-            $data['errors'] = $schedfare->errors;
-        }
+        $fareinstance = new Fareinstance();
+        $fares = $fareinstance->getFareInstances();
 
-        $this->view('schedfare', ['schedfares' => $schedfares]);
-    }
 
-    public function api_delete()
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // Retrieve the POST data
-            $postData = json_decode(file_get_contents('php://input'), true);
+        $halt = new Halt();
+        $halts = $halt->getHalts();
+        $data['halts'] = $halts;
+        $data['fares'] = $fares;
 
-            // Process the request data and perform the update
-            $fare = new Fare();
-            $id = $postData['id'];
-            $fare->deleteFares($id);
+        $data['username'] = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
+        $this->view('schedfare', $data);
+
         
-            // Send a response
-            $response = array('status' => 'success', 'data' => $postData);
-            header('Content-Type: application/json');
-            echo json_encode($response);
-        } else {
-            $response = array('status' => 'error', 'data' => 'Invalid request');
-            header('Content-Type: application/json');
-            echo json_encode($response);
-        }
+        
     }
+
+    // public function api_delete()
+    // {
+    //     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //         // Retrieve the POST data
+    //         $postData = json_decode(file_get_contents('php://input'), true);
+
+    //         // Process the request data and perform the update
+    //         $fare = new Fare();
+    //         $id = $postData['id'];
+    //         $fare->deleteFares($id);
+        
+    //         // Send a response
+    //         $response = array('status' => 'success', 'data' => $postData);
+    //         header('Content-Type: application/json');
+    //         echo json_encode($response);
+    //     } else {
+    //         $response = array('status' => 'error', 'data' => 'Invalid request');
+    //         header('Content-Type: application/json');
+    //         echo json_encode($response);
+    //     }
+    // }
 }
--- BusSched/app/views/schedfare.view.php
+++ BusSched/app/views/schedfare.view.php
@@ -20,9 +20,6 @@ if (!isset($_SESSION['USER'])) {
     <link href="<?= ROOT ?>/assets/css/schedfare.css" rel="stylesheet">
     <!-- <script src="<?= ROOT ?>/assets/js/schedulebreakdown.js">console.log("Hey")</script> -->
     <script src="<?= ROOT ?>/assets/js/schedbusfare.js">console.log("Hey")</script>
-
-    
-
 </head>
 
 <body>
@@ -41,176 +38,43 @@ if (!isset($_SESSION['USER'])) {
             <div><button class="button-grey add-btn">Add New</button></div>
         </div>
 
-        <!-- <form method="post" id="view_fare" style="display:none">
+        <div class="row">
+        <h1 style="margin-top:40px; color:#24315e; text-align:center;">A/C bus fares</h1>
+        <div class="fare-from-to-grid">
+            <input type="text" name="from" id="fare-from" placeholder="From" list="halt-list" required>
+            <input type="text" name="to" id="fare-to" placeholder="To" list="halt-list" required>
 
-            <?php if (!empty($errors)) : ?>
-                <?= implode("<br>", $errors) ?>
-            <?php endif; ?>
+            <button id="calculate-fare" class="button-orange">Find fare</button>
+            <div id="fare-result" class='span-3'></div>
+        </div>
+        <section id="busfare">
+            <div style="width:100%">
+                <table id="busfare-table">
+                    <?php
 
-            <div>
-                <table class="styled-table">
-                    <tr>
-                        <td><label for="source">From</label></td>
-                        <td><input type="text" class="form-control" name="source" id="source" placeholder="Starting halt..."></td>
-                    </tr>
+                    $len = count($halts);
+                    $fareinstance = new Fareinstance;
+                    $instance = $fareinstance->getFareInstances($len);
+                    
+                    for ($i = 0; $i < $len; $i++) {
+                        $halt = $halts[$i];
+                    ?>
+                        <tr data-haltfrom='<?=$halt->name?>'><td class='halt-name'><?=$halt->name?></td>
+                        <?php
+                        for ($j = 0; $j <= $i; $j++)
+                            { if ($i == $j) {?>
 
-                    <tr>
-                        <td><label for="dest">To</label></td>
-                        <td>
-                            <input type="text" name="dest" class="form-control" id="dest" placeholder="Destination halt...">
-                        </td>
-                    </tr>
+                            <td class='halt-name-top'><?=$halt->name?></td>
 
-                    <tr>
-                        <td><label for="route_bus">Route</label></td>
-                        <td><input type="text" name="route_bus" class="form-control" id="route_bus" placeholder="Bus route..."></td>
-                    </tr>
+                        <?php } else {?>
 
-                    <tr>
-                        <td><label for="type_bus">Type </label></td>
-                        <td>
-                            <select name="type_bus" id="type_bus" class="form-control">
-                                <option disabled selected value>--select an option--</option>
-                                <option value="L">Luxury</option>
-                                <option value="S">Semi-Luxury</option>
-                            </select>
-                        </td>
-                    </tr>
+                            <td class='fare-td' data-haltto='<?=$halts[$j]->name?>'><?=$instance[$i-$j]->fare?></td>
 
-                    <tr>
-                        <td><label for="amount">Amount</label></td>
-                        <td><input type="number" name="amount" class="form-control" id="amount" placeholder="Bus fare..."></td>
-                    </tr>
-
-                    <tr>
-                        <td></td>
-                        <td align="right">
-                            <button class="button-green" type="submit">Save</button>
-                            <button class="button-cancel" onclick="cancel()">Cancel</button>
-                        </td>
-                    </tr>
-
+                        <?php }}}?>
                 </table>
             </div>
-        </form>
-
-        <div>
-            <br>
-            <table border='1' class="styled-table" style="border-radius: 40px;">
-                <tr>
-                    <th>#</th>
-                    <th>From</th>
-                    <th>To</th>
-                    <th>Route</th>
-                    <!-- <th>Type</th> -->
-                    <th>Instance</th>
-                    <th>Amount</th>
-                    <th>Last Updated</th>
-                    <th>Action</th>
-                    <!-- <th>Action</th> -->
-                </tr>
-                <?php
-                  print_r($schedfares);
-                ?>
-                <!-- <?php
-                
-                foreach ($schedfares as $fare): ?> 
-                    <tr>
-                    <td> <?php echo $fare->id ?></td>
-                    <td> <?php echo $fare->instance ?></td>
-                    <td> <?php echo $fare->fare ?></td>
-                    
-                    <td> <?php echo $fare->last_updated ?></td>
-                    <td>
-                        <div class='edit_delete'> 
-                                <img src='<?= ROOT ?>/assets/images/icons/edit.png'>
-                                <img class = "delete-btn" src='<?=ROOT ?>/assets/images/icons/delete.png'>
-                        </div>      
-                    </td>
-                    </tr>
-                <?php endforeach ?>; 
-
-                <!-- <?php static $i = 1; ?>
-                <?php if ($schedfares):
-               foreach ($schedfares as $schedfare): ?>
-                <tr data-id=<?= $schedfare->id ?>>
-                    <td> <?= $i ?> </td>
-                    <?php $i++; ?>
-                    <td data-fieldname="source"> <?= $schedfare->source ?> </td>
-                    <td data-fieldname="dest"> <?= $schedfare->dest ?> </td>
-                    <td data-fieldname="bus_route"> <?= $schedfare->bus_route ?> </td>
-                    <td data-fieldname="amount"> <?= $schedfare->amount ?> </td>
-                    <td data-fieldname="last updated"> <?= $schedfare->last_updated ?> </td>
-
-                    <td id="edit-delete"> 
-                    <img src='<?= ROOT ?>/assets/images/icons/edit.png'  width='20px' height='20px'>
-                      <img src='<?= ROOT ?>/assets/images/icons/delete.png' alt='delete' class="icon delete-btn" width='20px' height='20px'>
-                    </td>
-                  </tr>
-              <?php endforeach; else: ?>
-                <tr>
-                  <td colspan="9" style="text-align:center;color:#999999;"><i>No fares found.</i></td>
-                </tr>
-        <?php endif; ?>
-
-        <tr class='dummy-input' >
-            <td></td>
-            <td data-fieldname="source">
-              <input type="text" value="" placeholder="source">
-            </td>
-            <td data-fieldname="dest">
-              <input type="text" value="" placeholder="dest">
-            </td>
-            <td data-fieldname="bus_route">
-              <input type="text" value="" placeholder="Route">
-            </td>
-            <td data-fieldname="amount">
-              <input type="text" value="" placeholder="amount">
-            </td>
-            <td data-fieldname="last_updated">
-              <input type="text" value="" placeholder="last_updated">
-            </td>
-            <!-- <td data-fieldname="dest">
-              <input type="text" value="" placeholder="Destination">
-            </td>
-            <td data-fieldname="owner">
-              <input type="text" value="" placeholder="Owner">
-            </td>
-            <td data-fieldname="conductor">
-              <input type="text" value="" placeholder="Conductor">
-            </td>
-            <td data-fieldname="driver">
-              <input type="text" value="" placeholder="Driver">
-            </td> -->
-            <!-- <td class='edit-options'>
-              <img src='<?= ROOT ?>/assets/images/icons/save.png' alt='save' class="icon save-btn" width='20px' height='20px'>
-              <img src='<?= ROOT ?>/assets/images/icons/cancel.png' alt='cancel' class="icon cancel-btn" width='20px' height='20px'>
-            </td>
-            <td class='add-options'>
-              <img src='<?= ROOT ?>/assets/images/icons/save.png' alt='save' class="icon add-row-btn" width='20px' height='20px'>
-              <img src='<?= ROOT ?>/assets/images/icons/cancel.png' alt='cancel' class="icon cancel-add-btn" width='20px' height='20px'>
-            </td>
-          </tr>
-          <tr class='dummy-row' style="display: none;">
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td id="edit-delete">
-              <img src='<?= ROOT ?>/assets/images/icons/edit.png' alt='edit' class="icon edit-btn" width='20px' height='20px'>
-              <img src='<?= ROOT ?>/assets/images/icons/delete.png' alt='delete' class="icon delete-btn" width='20px' height='20px'>
-            </td>
-          </tr> --> 
-
-            </table>
+        </section>
         </div>
-       
 
 
         <script src="<?= ROOT ?>/assets/js/bus.js"></script>
