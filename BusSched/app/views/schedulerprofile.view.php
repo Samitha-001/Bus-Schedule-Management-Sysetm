1b2b18213acbdb9197f4124aa5ab947f4db4fcbe
Samitha <rathnayakesamitha1317@gmail.com>
2023-05-13 16:30:29 +0530

    schedule function

--- BusSched/app/views/schedulerprofile.view.php
+++ BusSched/app/views/schedulerprofile.view.php
@@ -27,18 +27,120 @@ if (isset($_SESSION['USER'])) {
     <title>Profile</title>
     <style>
       td input[disabled] {
-        border: none;
-        background-color: transparent;
-        padding: 0;
-        margin: 0;
-        font-size: inherit;
-        font-family: inherit;
-        color: inherit;
-        cursor: default;
-      }
-      form.info-grid {
-        display: none;
-      }
+            border: none;
+            background-color: transparent;
+            padding: 0;
+            margin: 0;
+            font-size: inherit;
+            font-family: inherit;
+            color: inherit;
+            cursor: default;
+        }
+
+        form.info-grid {
+            display: none;
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            width: 100%;
+        }
+
+        .passenger-profile-card {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            background-color: white;
+            border-radius: 15px;
+            box-shadow: 0px 0px 5px grey;
+            width: 80%;
+            height: 80%;
+            padding: 30px;
+            margin: auto;
+        }
+
+        #profile-header {
+            flex-direction: column;
+        }
+
+        #profile-pic {
+            border-radius: 50%;
+            margin-right: 30px;
+            object-fit: cover;
+            width: 200px;
+            height: 200px;
+        }
+
+        #username {
+            margin-top: 0;
+        }
+
+        #passenger-info-div {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: flex-start;
+            width: 50%;
+            padding: 10px;
+        }
+
+        #edit-passenger-info {
+            align-self: center;
+            margin-top: 10px;
+        }
+
+        .edit-info-form {
+            display: none;
+            width: 50%;
+            padding: 10px;
+        }
+
+        .edit-info-form input {
+            width: 100%;
+            margin-bottom: 10px;
+        }
+
+        .info-grid-start-2 {
+            display: grid;
+            grid-template-columns: 1fr 1fr auto auto;
+            justify-items: start;
+            align-items: center;
+            margin-top: 10px;
+        }
+
+        .info-grid-start-2 span {
+            grid-column: 1 / 3;
+        }
+
+        #profile-pic-from-div {
+            display: none;
+        }
+
+        @media (max-width: 768px) {
+            .passenger-profile-card {
+                flex-direction: column;
+                width: 90%;
+                height: auto;
+                padding: 20px;
+            }
+
+            #profile-pic {
+                margin-right: 0;
+                margin-bottom: 20px;
+            }
+
+            #passenger-info-div {
+                width: 100%;
+            }
+
+            .edit-info-form {
+                width: 100%;
+            }
+        }
     </style>
 </head>
 
@@ -49,7 +151,7 @@ if (isset($_SESSION['USER'])) {
 
         $scheduler = $data[0];
         $username = $scheduler->username;
-        $schednew1 = new Scheduler();
+       
     ?>
 
 <datalist id="passenger-list">
@@ -67,58 +169,74 @@ if (isset($_SESSION['USER'])) {
         ?>
     </datalist>
 
-        <div class="passenger-profile-card" id="profile-header">
-        <img id="profile-picture" src="<?= ROOT ?>/assets/images/icons/profile-pic-none.png" alt="profile pic" width="50px" height="50px">
+    <div class="container">
+    <div class="passenger-profile-card" id="profile-header">
+        <?php
+        // get profile picture if exists, else use default
+        $pics = "assets/images/profile-pics/" . $username . ".*";
+        $profile_pics = glob($pics);
+        if (count($profile_pics) > 0) {
+            $profile_pic = $profile_pics[0];
+        } else {
+            $profile_pic = "assets/images/icons/profile-pic-none.png";
+        }
+        ?>
+
+        <img id="profile-pic" src="<?= $profile_pic ?>" alt="profile pic" width="200px" height="200px">
+        <img id="edit-pencil" src="<?= ROOT ?>/assets/images/icons/edit-pencil.png">
+
         <h1 id="username">Hi <?= $username ?>!</h1>
-    </div>
-    
 
-        <div class="row">
-        
-        <div class="nav-cards">
-        <div class="passenger-profile-card">
-            <div class="info-grid" style="padding-left:5px;">
-                <h1>Username:</h1>
-                <p data-username="<?= $username ?>">
-                    <?= $scheduler->username ?>
-                </p>
-                <h1>Name:</h1>
-                <p>
-                    <?= $scheduler->name ?>
-                </p>
-                
-                <h1>Phone:</h1>
-                <p>
-                    <?= $scheduler->phone ?>
-                </p>
-                <h1>Address:</h1>
-                <p>
-                    <?= $scheduler->address ?>
-                </p>
-                
-                <button href=# id='edit-passenger-info' style="background-color: #24315e;">Edit</button>
+        <div id="passenger-info-div">
+            <h5>Name</h5>
+            <hr>
+            <p data-username="<?= $username ?>">
+                <?= $scheduler->name ?>
+            </p>
+            <h5>Phone</h5>
+            <hr>
+            <p>
+                <?= $scheduler->phone ?>
+            </p>
+            <h5>Address</h5>
+            <hr>
+            <p>
+                <?= $scheduler->address ?>
+            </p>
+            <br>
+            <button href=# id='edit-passenger-info'>Edit profile</button>
+        </div>
+
+        <!-- edit form for passenger info -->
+        <form class='edit-info-form'>
+            <h5>Name</h5>
+            <hr>
+            <input type="text" name="name" id="name" value="<?= $scheduler->name ?>">
+            <h5>Phone</h5>
+            <hr>
+            <input type="text" name="phone" id="phone" value="<?= $scheduler->phone ?>">
+            <h5>Address</h5>
+            <hr>
+            <input type="text" name="address" id="address" value="<?= $scheduler->address ?>">
+            <br>
+
+            <div class="info-grid-start-2">
+                <span></span>
+                <span></span>
+                <button id='cancel-passenger-info'>Cancel</button>
+                <button id='save-passenger-info'>Save</button>
             </div>
-
-            <!-- edit form for passenger info -->
-            <form method="post" action="<?ROOT ?>/schedulerprofile/api_edit" style="padding-left:5px;" class='edit-info-form info-grid'>
-            <h1>Username:</h1>
-                <input type="text" name="username" id="username" value="<?= $scheduler->username ?>">
-                <h1>Name:</h1>
-                <input type="text" name="name" id="name" value="<?= $scheduler->name ?>">
-                <h1>Phone:</h1>
-                <input type="text" name="phone" id="phone" value="<?= $scheduler->phone ?>">
-                <h1>Address:</h1>
-                <input type="text" name="address" id="address" value="<?= $scheduler->address ?>">
-                
-                
-                <!-- TODO -->
-                <div class="info-grid-start-2">
-                    <button id='save-scheduler-info' style="background-color: #24315e;" type="submit">Save</button>
-                    <button id='cancel-passenger-info' style="background-color: #24315e;">Cancel</button>
-                </div>
+        </form>
+        <!-- div to add profile picture -->
+        <div class="passenger-profile-card" id="profile-pic-from-div" style="display:none;">
+            <form id="profile-pic-form">
+                <input type="file" name="profile-pic" id="profile-pic-input" accept="image/*">
+                <input type="hidden" name="username" id="username" value="<?= $username ?>">
+                <button id="upload-profile-pic-btn">Upload</button>
             </form>
+        </div>
 
-        </div>
-        </div>
+        <!-- input to get image file as profile picture -->
+    </div>
 
 </body>
--- BusSched/public/assets/js/schedulerprofile.js
+++ BusSched/public/assets/js/schedulerprofile.js
@@ -1,118 +1,304 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let editbtn = document.getElementById("edit-passenger-info");
   
-    editbtn.addEventListener("click", function (e) {
-      editRow(e);
-    });
   
-    // save passenger info edit
-    let savebtn = document.getElementById("save-scheduler-info");
-    // event listener to save button
-    savebtn.addEventListener("click", function (e) {
-      e.preventDefault();
-      saveRow(e);
-    });
+  // GET PROFILE PICTURE from form
+  let profilePicForm = document.getElementById("profile-pic-form");
+  let profilePicEditBtn = document.getElementById("edit-pencil");
   
-    // cancel passenger info edit
-    let cancelbtn = document.getElementById("cancel-scheduler-info");
+  let editbtn = document.getElementById("edit-passenger-info");
+
+  editbtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    editRow(e);
+  });
+
+  // save passenger info edit
+  let savebtn = document.getElementById("save-passenger-info");
+  // event listener to save button
+  savebtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    saveRow(e);
+  });
+
+  // cancel passenger info edit
+  let cancelbtn = document.getElementById("cancel-passenger-info");
+
+  // add event listener to cancel button
+  cancelbtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    cancelEdit(e);
+  });
+
+  // function to autofill and display editing row
+  function editRow(e) {
+    let ticketdiv = e.target.parentElement.parentElement;
+    let userdetails = ticketdiv.querySelectorAll("p");
+    let name = userdetails[0].textContent.trim();
+    let phone = userdetails[1].textContent.trim();
+    let address = userdetails[2].textContent.trim();
+    
+
+    // get the form inside ticketdiv
+    let infoform = ticketdiv.querySelector("form");
+
+    // get element by class
+    let profileinfo = ticketdiv.querySelector("div#passenger-info-div");
+
+    // inputs
+    let inputs = infoform.querySelectorAll("input");
+
+    inputs[0].value = name;
+    inputs[1].value = phone;
+    inputs[2].value = address;
+   
+
+    profileinfo.style.display = "none";
+    infoform.style.display = "block";
+    profilePicEditBtn.style.display = "block";
+  }
+
+  // cancel edit
+  function cancelEdit(e) {
+    let infoform = e.target.parentElement.parentElement;
+    infoform.style.display = "none";
+
+    let ticketdiv = e.target.parentElement.parentElement.parentElement;
+    let profileinfo = ticketdiv.querySelector("#passenger-info-div");
+    profileinfo.style.display = "block";
+    profilePicEditBtn.style.display = "none";
+  }
+
+  // save edited info
+  function saveRow(e) {
+    // get div
+    let ticketdiv = e.target.parentElement.parentElement.parentElement;
+    let prevValues = ticketdiv.querySelectorAll("p");
+    let id = document.querySelector("#username").innerHTML;
   
-    // add event listener to cancel button
-    cancelbtn.addEventListener("click", function (e) {
-      cancelEdit(e);
-    });
-  
-    // function to autofill and display editing row
-    function editRow(e) {
-      let ticketdiv = e.target.parentElement.parentElement;
-      let userdetails = ticketdiv.querySelectorAll("p");
-      let username = userdetails[0].textContent.trim();
-      let name = userdetails[1].textContent.trim();
-      let phone = userdetails[2].textContent.trim();
-      let address = userdetails[3].textContent.trim();
-      
-  
-      // get the form inside ticketdiv
-      let infoform = ticketdiv.querySelector("form");
-  
-      // get element by class
-      let profileinfo = ticketdiv.querySelector("div.info-grid");
-  
-      // inputs
-      let inputs = infoform.querySelectorAll("input");
-      inputs[0].value = username;
-      inputs[1].value = name;
-      inputs[2].value = phone;
-      inputs[3].value = address;
-      
-  
-      profileinfo.style.display = "none";
-      infoform.style.display = "grid";
+    // get username from above
+    id = id.substring(3, id.length - 1);
+    let data = { id: id };
+    let inputs = ticketdiv.querySelectorAll("input");
+
+    // check values of inputs against previous values
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].value !== prevValues[i].textContent.trim()) {
+        data[inputs[i].name] = inputs[i].value;
+      }
     }
-  
-    // cancel edit
-    function cancelEdit(e) {
-      let infoform = e.target.parentElement.parentElement;
-      infoform.style.display = "none";
-  
-      let ticketdiv = e.target.parentElement.parentElement.parentElement;
-      let profileinfo = ticketdiv.querySelector(".info-grid");
-      profileinfo.style.display = "grid";
+
+    // check if data is not empty
+    if (Object.keys(data).length !== 0) {
+      // send data to server
+      let url = `${ROOT}/passengerprofile/api_edit`;
+      let options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data);
+          for (let i = 0; i < inputs.length; i++) {
+            prevValues[i].textContent = inputs[i].value;
+          }
+          // hide form and show info
+          let infoform = ticketdiv.querySelector("form");
+          infoform.style.display = "none";
+          let profileinfo = ticketdiv.querySelector("#passenger-info-div");
+          profileinfo.style.display = "block";
+          profilePicEditBtn.style.display = "none";
+          // if (data.success) {
+          //     // this doesn't get called
+          //     // update the values
+          // } else {
+          //     // alert(data.message);
+          // }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  }
+
+  // GIFT POINTS
+  let giftPointsDiv = document.getElementById("gift-points-div");
+  let giftPointsBtn = document.getElementById("gift-points-btn");
+  let cancelGiftbtn = document.getElementById("cancel-gift-btn");
+  let confirmGiftbtn = document.getElementById("confirm-gift-btn");
+  let pointBalanceDiv = document.getElementById("point-balance-div");
+
+  cancelGiftbtn.addEventListener("click", function (e) {
+    giftPointsDiv.style.display = "none";
+    giftPointsBtn.style.display = "block";
+  });
+
+  giftPointsBtn.addEventListener("click", function (e) {
+    giftPointsDiv.style.display = "block";
+    giftPointsBtn.style.display = "none";
+  });
+
+  confirmGiftbtn.addEventListener("click", function (e) {
+    //check if gifting points > current value
+    let g = document.querySelector('#points').value
+    let c = parseFloat(pointBalanceDiv.querySelector('p').innerHTML)
+    if (g>c){
+      new Toast('fa fa fa-exclamation-triangle', '#ff0000','Invalid','You cannot gift more points than you have',true,3000)
+      return
+    }
+    let confirm = window.confirm("Are you sure you want to gift points?");
+    if (confirm) {
+      giftPointsDiv.style.display = "none";
+      giftPointsBtn.style.display = "block";
+      // change points
+      // call function to gift points
+      giftPoints();
+    }
+  });
+
+  function giftPoints(e) {
+    let inputs = giftPointsDiv.querySelectorAll("input");
+    // option selected from select
+    let select = giftPointsDiv.querySelector("select").value;
+    let data = {};
+
+    // if empty
+    if (select === "") {
+      alert("Please select a passenger to gift points to");
+      return;
+    }
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (!inputs[i].value) {
+        alert("Please enter points to gift");
+        return;
+      }
+      data[inputs[i].name] = inputs[i].value;
+    }
+    data["points_to"] = select;
+    
+    // update point balance
+    let vals = pointBalanceDiv.querySelectorAll("p");
+    
+    vals[0].textContent = parseInt(vals[0].textContent) - parseInt(data["amount"]);
+    vals[1].textContent = parseInt(vals[1].textContent) - parseInt(data["amount"]) + " LKR";
+
+    // send data to server
+    let url = `${ROOT}/passengerprofile/api_gift_points`;
+    let options = {
+      method: "POST",
+      credentials: "same-origin",
+      mode: "same-origin",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(url, options)
+      .then((response) => response.json())
+      .catch((err) => {
+        console.log(err);
+      })
+      .then((data) => {
+        // console.log(data);
+      });
+  }
+
   
-    // save edited info
-    function saveRow(e) {
-      // get div
-      let ticketdiv = e.target.parentElement.parentElement.parentElement;
-      let prevValues = ticketdiv.querySelectorAll("p");
-      let id = document.querySelector("#username").innerHTML;
-      // Hi scheduler1!
-      //get username from above
-      id = id.substring(3, id.length - 1);
-      let data = { id: id };
-      let inputs = ticketdiv.querySelectorAll("input");
-  
-      // check values of inputs against previous values
-      for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value !== prevValues[i].textContent.trim()) {
-          data[inputs[i].name] = inputs[i].value;
+  // let profilePicInput = document.getElementById("profile-pic-input");
+  // value of input with name username
+  let username = profilePicForm.querySelector("input[name='username']").value;
+  let profilePicUploadBtn = document.getElementById("upload-profile-pic-btn");
+
+  profilePicEditBtn.addEventListener("click", function (e) { 
+    document.getElementById("profile-pic-from-div").style.display = "block";
+  });
+
+  profilePicUploadBtn.addEventListener("click", function (e) {
+    // prevent default
+    e.preventDefault();
+
+    // hide form
+    let profilePic = document.getElementById("profile-pic");
+    profilePic.src = `${ROOT}/assets/images/profile-pics/${username}.jpg`;
+    document.getElementById("profile-pic-from-div").style.display = "none";
+
+    // save file in uploads folder
+    let formData = new FormData(profilePicForm);
+    let url = `${ROOT}/passengerprofile/api_upload_profile_pic`;
+    let options = {
+      method: "POST",
+      body: formData,
+    };
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // redirect to profile page
+          window.location.href = `${ROOT}/passengerprofile`;
+          
+        } else {
+          alert(data.message);
         }
       }
-  
-      // check if data is not empty
-      if (Object.keys(data).length !== 0) {
-        // send data to server
-        let ROOT = 'https://www.yourwebsite.com';
-        let url = `${ROOT}/schedulerprofile/api_edit`;
-        let options = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        };
-        fetch(url, options)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-            for (let i = 0; i < inputs.length; i++) {
-              prevValues[i].textContent = inputs[i].value;
-            }
-            // hide form and show info
-            let infoform = ticketdiv.querySelector("form");
-            infoform.style.display = "none";
-            let profileinfo = ticketdiv.querySelector(".info-grid");
-            profileinfo.style.display = "grid";
-            // if (data.success) {
-            //     // this doesn't get called
-            //     // update the values
-            // } else {
-            //     // alert(data.message);
-            // }
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    }  
+      )
   });
+
+  // add friend
+  let addFriendBtn = document.getElementById("add-friend-btn");
+  let addFriendDiv = document.getElementById("add-friend-div");
+  let cancelAddFriendBtn = document.getElementById("cancel-add-friend-btn");
+  let confirmAddFriendBtn = document.getElementById("confirm-add-friend-btn");
+
+  addFriendBtn.addEventListener("click", function (e) {
+    addFriendDiv.style.display = "block";
+    addFriendBtn.style.display = "none";
+  });
+
+  cancelAddFriendBtn.addEventListener("click", function (e) {
+    addFriendDiv.style.display = "none";
+    addFriendBtn.style.display = "block";
+  });
+
+  confirmAddFriendBtn.addEventListener("click", function (e) {
+    let confirm = window.confirm("Are you sure you want to add friend?");
+    if (confirm) {
+      addFriendDiv.style.display = "none";
+      addFriendBtn.style.display = "block";
+      
+      addFriend();
+      // display data in friend list
+      // create div
+      let div = document.createElement("div");
+      // add i tag inside div
+      let i = document.createElement("i");
+      i.innerHTML = "Remove friend";
+      i.classList.add("remove-friend-i");
+      // add attribute
+      let input = document.getElementById("friend");
+
+      i.setAttribute("data-friend", input.value);
+      div.appendChild(i);
+      let p = document.createElement("p");
+      p.innerHTML = "@"+input.value;
+      div.appendChild(p);
+      div.appendChild(document.createElement("hr"));
+
+      // add div to friend list
+      friendListDiv.appendChild(div);
+    }
+  });
+
   
+
+  // remove friend
+  
+
+
+  // function to remove a friend
+  
+});
