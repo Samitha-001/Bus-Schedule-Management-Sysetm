4ee984f00fd2d802c2b50717b604db04b7cc674b
Venudi <63283896+Venudi@users.noreply.github.com>
2023-05-14 23:50:58 +0530

    Owner breakdown

--- BusSched/app/controllers/Ownerbreakdowns.php
+++ BusSched/app/controllers/Ownerbreakdowns.php
@@ -45,4 +45,36 @@ class OwnerBreakdowns
         }
     }
 
+    // repair breakdown api
+    public function api_repair_breakdown() 
+    {
+        if($_SERVER['REQUEST_METHOD'] == "POST") {
+            // retrieve the POST data
+            $postData = json_decode(file_get_contents('php://input'), true);
+
+            $breakdown = new Breakdown();
+            $breakdown->repairBreakdown($postData['breakdownID']);
+
+            // Send a response
+            $response = array('status' => 'success', 'data' => $postData);
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        } else {
+            $response = array('status' => 'error', 'data' => 'Invalid requests');
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
+    }
+
+    
+    // function to modify breakdown
+    public function modifyBreakdown($id) {
+        $breakdown = new Breakdown();
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $breakdown->update($id, ['time_to_repair'=>$_POST['time_to_repair']]);
+            show($id);
+            // redirect('ownerbreakdowns');
+        }
+    }
 }
