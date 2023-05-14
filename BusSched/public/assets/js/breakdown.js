b2cdcaac5086f09f35b16d5c78b70bfce5cd26da
Venudi <63283896+Venudi@users.noreply.github.com>
2023-05-14 14:27:48 +0530

    edit breakdown conductor

--- BusSched/public/assets/js/breakdown.js
+++ BusSched/public/assets/js/breakdown.js
@@ -1,141 +1,91 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   // get breakdown-repaired-btn button
   const btn = document.getElementById("breakdown-repaired-btn");
-  console.log(btn);
 
-  // add event listener to button
-  btn.addEventListener("click", () => {
-    // get breakdown id
-    const breakdownId = btn.getAttribute("data-breakdown-id");
-    
-    let data = {
-      breakdownID: breakdownId,
-    };
+  // if button exists
+  if (btn) {
+    // add event listener to button
+    btn.addEventListener("click", () => {
+      // get breakdown id
+      const breakdownId = btn.getAttribute("data-breakdown-id");
+      
+      let data = {
+        breakdownID: breakdownId,
+      };
 
-    // send data to the api
-    let url = `${ROOT}/conductorbreakdowns/api_repair_breakdown`;
-    let options = {
-      method: "POST",
-      credentials: "same-origin",
-      mode: "same-origin",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(data),
-    };
+      // send data to the api
+      let url = `${ROOT}/conductorbreakdowns/api_repair_breakdown`;
+      let options = {
+        method: "POST",
+        credentials: "same-origin",
+        mode: "same-origin",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(data),
+      };
 
-    fetch(url, options)
-      .then((res) => res.json())
-      .catch((error) => {
-        console.log(error);
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  });
-
-  // const btn = document.getElementById("btn");
-  // const btn2 = document.getElementById("btn2");
-  // let noBreakdown = document.getElementById("no-breakdowns-td");
-  // if (noBreakdown) {
-  //   btn.disabled = false;
-  // } else {
-  //   btn.disabled = true;
-  // }
+      fetch(url, options)
+        .then((res) => res.json())
+        .catch((error) => {
+          console.log(error);
+        })
+        .then((data) => {
+          console.log(data);
+          window.location.reload();
+        });
+    });
+  }
 
 
-  // btn.addEventListener("click", () => {
-  //   const form = document.getElementById("view_route");
-
-  //   if (form.style.display === "none") {
-  //     form.style.display = "block";
-  //   } else {
-  //     form.style.display = "none";
-  //   }
-  // });
-
-  // btn.addEventListener("click", () => {
-  //   const form = document.getElementById("view_fare");
-
-  //   if (form.style.display === "none") {
-  //     form.style.display = "block";
-  //   } else {
-  //     form.style.display = "none";
-  //   }
-  // });
-
-  // btn.addEventListener("click", () => {
-  //   const form = document.getElementById("view_registernewbus");
-
-  //   if (form.style.display === "none") {
-  //     form.style.display = "block";
-  //   } else {
-  //     form.style.display = "none";
-  //   }
-  // });
-
-
-  // btn.addEventListener("click", () => {
-  //   const form = document.getElementById("view_breakdown");
-  //   if (form.style.display === "none") {
-  //     form.style.display = "block";
-  //   } else {
-  //     form.style.display = "none";
-  //   }
-  // });
-
-
-  // btn2.addEventListener("click", () => {
-  //   const form = document.getElementById("view_my_breakdowns");
-  //   if (form.style.display === "none") {
-  //     form.style.display = "block";
-  //   } else {
-  //     form.style.display = "none";
-  //   }
-  // });
-
-  // const btn_history = document.getElementById("btn_history");                                            
-  // btn_history.addEventListener("click", () => {
-  //   const table = document.getElementById("view_history_breakdowns");
+  // get breakdown-edit-btn button
+  const editBtn = document.getElementById("breakdown-edit-btn");
+  const saveBtn = document.getElementById("breakdown-save-btn");
+  const cancelBtn = document.getElementById("breakdown-cancel-btn");
   
-  //   if (table.style.display === "none") {
-  //     table.style.display = "block";
-  //   } else {
-  //     table.style.display = "none";
-  //   }
-  // });
-  
+  // if button exists
+  if (editBtn) {
+    // add event listener
+    editBtn.addEventListener("click", () => {
+      // console.log(editBtn);
+      // get #repair-time input
+      const repairTimeInput = document.getElementById("repair-time");
+      repairTimeInput.disabled = false;
 
-  // function cancel() {
-  //   const form = document.getElementById("view_bus");
-  //   form.style.display = "none";
-  // }
+      saveBtn.style.display = "inline-block";
+      cancelBtn.style.display = "inline-block";
+      btn.style.display = "none";
+      editBtn.style.display = "none";
 
-  // function cancel() {
-  //   const form = document.getElementById("view_fare");
-  //   form.style.display = "none";
-  // }
+    });
+  }
 
-  // function cancel() {
-  //   const form = document.getElementById("view_breakdown");
-  //   form.style.display = "none";
-  // }
+  // if cancel button exists
+  if (cancelBtn) {
+    // add event listener
+    cancelBtn.addEventListener("click", () => {
+      // get #repair-time input
+      const repairTimeInput = document.getElementById("repair-time");
+      repairTimeInput.disabled = true;
 
-  // function cancel() {
-  //   const form = document.getElementById("view_route");
-  //   form.style.display = "none";
-  // }
+      saveBtn.style.display = "none";
+      cancelBtn.style.display = "none";
+      btn.style.display = "inline-block";
+      editBtn.style.display = "inline-block";
+    });
+  }
 
-  // const editButton = document.getElementById('edit-breakdown-info');
-  // const editFormContainer = document.getElementById('edit-form-container');
-  // // const cancelButton = document.getElementById('cancel-breakdown-info');
 
-  // editButton.addEventListener('click', () => {
-  //   editFormContainer.style.display = 'block';
-  // });
+  // if save button exists
+  if (saveBtn) {
+    // get submit edit button
+    const submitEditBtn = document.getElementById("submit-edit-btn");
+    // add event listener
+    saveBtn.addEventListener("click", (e) => {
+      // click the submit button
+      submitEditBtn.click();
+    });
 
-  // // cancelButton.addEventListener('click', () => {
-  // //   editFormContainer.style.display = 'none';
-  // // });
-
+  }
 });
