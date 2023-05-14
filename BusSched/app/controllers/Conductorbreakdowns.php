d2d378c90d35a7679960c3d4bbd693b3d64c8cb5
Venudi <venudidisara@gmail.com>
2023-05-14 23:27:35 +0530

    Add breakdown api + calling

--- BusSched/app/controllers/Conductorbreakdowns.php
+++ BusSched/app/controllers/Conductorbreakdowns.php
@@ -13,21 +13,20 @@ class Conductorbreakdowns
     }
 
     // function to add new breakdown
-    public function addBreakdown() {
-        $breakdown = new Breakdown();
-        $data = [];
-
+    public function api_add_breakdown(){
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            $_POST['bus_no'] = strtoupper($_POST['bus_no']);
-            if ($breakdown->validate($_POST)) {
-                $breakdown->addBreakdown($_POST);
-
-                redirect('breakdowns');
-            }
-
-            $data['errors'] = $breakdown->errors;
+            $post = json_decode(file_get_contents('php://input'), true);
+            $trip = $post['trip_id'];
+            $bus = $post['bus_no'];
+            $description = $post['description'];
+            $time_to_repair = $post['time_to_repair'];
+            (new Breakdown())->addBreakdown(['bus_no'=>$bus,'description'=>$description,'time_to_repair'=>$time_to_repair],$trip);
+            $response = array('status' => 'success', 'data' => $post);
+            header('Content-Type: application/json');
+            echo json_encode($response);
         }
     }
+
 
     // function to call when breakdown is repaired
     public function repairBreakdown($id) {
