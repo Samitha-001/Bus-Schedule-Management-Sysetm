7f9643d49df852e03f03fc04fab18d32e21dc5c3
Venudi <63283896+Venudi@users.noreply.github.com>
2023-05-14 13:02:54 +0530

    conductor trips update location api

--- BusSched/app/controllers/Conductortrips.php
+++ BusSched/app/controllers/Conductortrips.php
@@ -105,5 +105,35 @@ class Conductortrips
         return $trip->addBreakdown($d, $tripno);
     }
 
+    // function to update trip location
+    public function api_update_location()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Retrieve the POST data
+            $postData = json_decode(file_get_contents('php://input'), true);
+            
+            // add, location update record
+            $location_update = new Location_update();
+            
+            $data = [
+                'username' => $postData['username'],
+                'user_role' => $postData['user_role'],
+                'tripID' => $postData['tripID'],
+                'halt' => $postData['halt'],
+                'timestamp' => date('Y-m-d H:i:s')
+            ];
+
+            $location_update->addLocationUpdate($data, $postData['user_role']);
+
+            // Send a response
+            $response = array('status' => 'success', 'data' => $postData);
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        } else {
+            $response = array('status' => 'error', 'data' => 'Invalid requests');
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
+    }
     
 }
